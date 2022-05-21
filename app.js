5db6b1979ed3eedde62affa237177d49fc66bf36
M-Fahad786 <fahad.zahid2004@gmail.com>
2022-05-21 17:43:54 +0500

    Js Project

---
+++ app.js
@@ -0,0 +1,26 @@
+alert(" Error! Please Enter a Valid Password");
+alert("Welcome To JS LAnd...");
+alert("Happy Coding");
+
+alert("PIZZA \n PIZZ \n PIZ \n PI \n P ");
+
+var email = "fahad.zahid2004@gmail.com";
+alert ("My Email Address is" + email);
+
+var  book = "A smarter way to learn JavaScript"
+alert(book);
+var age = "14 years old";
+alert("I am " + age);
+var track = "10"
+var username = prompt("What's your name?")
+alert("Hi," + username)
+
+var username = prompt("Please enter your city  ?")
+alert("Welcome to the city of lights")
+
+
+
+
+
+
+
